03bef4e3b50d11954f8163fe2937ccd7a90e6009
SUHARIK <sukhoveev.roma@gmail.com>
2023-06-17 17:53:13 +1000

    String random formats methods

--- src/main/java/ru/incrementstudio/incrementstudioapi/utils/StringUtil.java
+++ src/main/java/ru/incrementstudio/incrementstudioapi/utils/StringUtil.java
@@ -1,5 +1,7 @@
 package ru.incrementstudio.incrementstudioapi.utils;
 
+import java.util.Arrays;
+
 public class StringUtil {
     public static String repeat(String pattern, int count) {
         String result = "";
@@ -7,5 +9,31 @@ public class StringUtil {
             result += pattern;
         }
         return result;
+    }
+
+    public static int getIntFromString(String value) {
+        if (value.contains(" : ")) {
+            String[] values = value.split(" : ");
+            return Integer.parseInt((String) RandomUtil.getRandomFromList(Arrays.stream(values).toList()));
+        } else if (value.contains(" ! ")) {
+            String[] values = value.split(" ! ");
+            if (values.length != 2) return 0;
+            return RandomUtil.getInt(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
+        } else {
+            return Integer.parseInt(value);
+        }
+    }
+
+    public static double getDoubleFromString(String value) {
+        if (value.contains(" : ")) {
+            String[] values = value.split(" : ");
+            return Double.parseDouble((String) RandomUtil.getRandomFromList(Arrays.stream(values).toList()));
+        } else if (value.contains(" ! ")) {
+            String[] values = value.split(" ! ");
+            if (values.length != 2) return 0;
+            return RandomUtil.getDouble(Double.parseDouble(values[0]), Double.parseDouble(values[1]));
+        } else {
+            return Double.parseDouble(value);
+        }
     }
 }
