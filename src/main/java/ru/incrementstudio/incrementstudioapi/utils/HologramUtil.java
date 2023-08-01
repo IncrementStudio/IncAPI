794f2b6a5bd033c4bfe517f6ada668c15f054423
SUHARIK <sukhoveev.roma@gmail.com>
2023-08-01 21:44:36 +1000

    Update HologramUtil.java

--- src/main/java/ru/incrementstudio/incrementstudioapi/utils/HologramUtil.java
+++ src/main/java/ru/incrementstudio/incrementstudioapi/utils/HologramUtil.java
@@ -1,11 +1,13 @@
-package ru.incrementstudio.incairdrops;
+package ru.incrementstudio.incrementstudioapi.utils;
 
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class HologramUtil {
-    public static ArmorStand spawnHologram(Location location, String text) {
+    public static ArmorStand spawnHologram(Plugin plugin, Location location, String text) {
         ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
         armorStand.setCollidable(false);
         armorStand.setInvulnerable(true);
@@ -17,6 +19,16 @@ public class HologramUtil {
         armorStand.setGravity(false);
         armorStand.setCustomName(text);
         armorStand.setCustomNameVisible(true);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (armorStand.isDead()) {
+                    cancel();
+                    return;
+                }
+                armorStand.teleport(location);
+            }
+        }.runTaskTimer(plugin, 5L, 5L);
         return armorStand;
     }
 }
