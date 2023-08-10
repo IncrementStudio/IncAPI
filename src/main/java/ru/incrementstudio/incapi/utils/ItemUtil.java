ad1f9f977b02cc36e83e1d6fef2d6e6d61b4ec3f
ItzElite <semen.glushkov.08@mail.ru>
2023-08-10 23:57:21 +1000

    + new methon ItemUtil.java

--- src/main/java/ru/incrementstudio/incapi/utils/ItemUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/ItemUtil.java
@@ -2,12 +2,14 @@ package ru.incrementstudio.incapi.utils;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import ru.incrementstudio.incapi.configs.templates.EnchantmentTemplate;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -99,5 +101,24 @@ public class ItemUtil {
         itemMeta.setCustomModelData(customModelData);
         itemStack.setItemMeta(itemMeta);
         return itemStack;
+    }
+
+    public static ItemStack createItemStack(Material material, String name, int amount, List<String> lore, List<ItemFlag> flags, HashMap<String, String> persistentData, boolean coloredName, boolean coloredLore, boolean enchanted) {
+        ItemStack item = new ItemStack(material, amount);
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName(coloredName ? ColorUtil.toColor(name) : name);
+        List<String> editedLore = new ArrayList<>(lore);
+        itemMeta.setLore(coloredLore ? ColorUtil.toColor(editedLore) : editedLore);
+        for (int i = 0; i < flags.size(); i++) {
+            itemMeta.addItemFlags(flags.get(i));
+        }
+        for (String key : persistentData.keySet()) {
+            itemMeta.getPersistentDataContainer().set(NamespacedKey.fromString(key), PersistentDataType.STRING, persistentData.get(key));
+        }
+        if (enchanted) {
+            itemMeta.addEnchant(Enchantment.LUCK, 1, true);
+        }
+        item.setItemMeta(itemMeta);
+        return item;
     }
 }
