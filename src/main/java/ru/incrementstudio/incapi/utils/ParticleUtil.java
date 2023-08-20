fba6b7d029cbcbe73cd83febe34c3f62d8f66182
SUHARIK <sukhoveev.roma@gmail.com>
2023-08-21 06:27:46 +1000

    [!] Change ParticleUtil methods structure

--- src/main/java/ru/incrementstudio/incapi/utils/ParticleUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/ParticleUtil.java
@@ -4,34 +4,24 @@ import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 public class ParticleUtil {
-    public static void createParticleArea(Location center, double range, Particle particle, double chance, double step) {
-        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += step) {
-            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += step) {
-                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += step) {
-                    if (RandomUtil.getDouble(0, 100) <= chance) {
-                        Location particleLocation = new Location(center.getWorld(), x, y, z);
-                        if (particleLocation.distance(center) <= range) {
-                            center.getWorld().spawnParticle(particle, particleLocation, 1);
-                        }
-                    }
-                }
-            }
+    public static void createParticleArea(Location center, double range, Particle particle, int count) {
+        Location particleLocation;
+        for (int i = 0; i < count; i++) {
+            do {
+                particleLocation = new Location(center.getWorld(), RandomUtil.getDouble(-range, range), RandomUtil.getDouble(-range, range), RandomUtil.getDouble(-range, range));
+            } while (particleLocation.distance(center) > range);
+            center.getWorld().spawnParticle(particle, particleLocation, 1);
         }
     }
 
-    public static void createColoredParticleArea(Location center, double range, Particle particle, double chance, double step, Particle.DustOptions dustOptions, Color color) {
-        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += step) {
-            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += step) {
-                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += step) {
-                    if (RandomUtil.getDouble(0, 100) <= chance) {
-                        Location particleLocation = new Location(center.getWorld(), x, y, z);
-                        if (particleLocation.distance(center) <= range) {
-                            center.getWorld().spawnParticle(particle, particleLocation, 0,
-                                    color.getRed() / 255.0, color.getGreen() / 255.0, color.getBlue() / 255.0, dustOptions);
-                        }
-                    }
-                }
-            }
+    public static void createColoredParticleArea(Location center, double range, Particle particle, int count, Particle.DustOptions dustOptions, Color color) {
+        Location particleLocation;
+        for (int i = 0; i < count; i++) {
+            do {
+                particleLocation = new Location(center.getWorld(), RandomUtil.getDouble(-range, range), RandomUtil.getDouble(-range, range), RandomUtil.getDouble(-range, range));
+            } while (particleLocation.distance(center) > range);
+            center.getWorld().spawnParticle(particle, particleLocation, 0,
+                    color.getRed() / 255.0, color.getGreen() / 255.0, color.getBlue() / 255.0, dustOptions);
         }
     }
 }
