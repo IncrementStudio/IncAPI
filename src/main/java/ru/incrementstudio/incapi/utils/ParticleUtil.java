6aa514314302c5638349c25508282952eed29c00
SUHARIK <sukhoveev.roma@gmail.com>
2023-08-21 06:02:15 +1000

    [!] Small step in methods of ParticleUtil

--- src/main/java/ru/incrementstudio/incapi/utils/ParticleUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/ParticleUtil.java
@@ -5,9 +5,9 @@ import org.bukkit.Location;
 import org.bukkit.Particle;
 public class ParticleUtil {
     public static void createParticleArea(Location center, double range, Particle particle, double chance) {
-        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += 0.5) {
-            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += 0.5) {
-                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += 0.5) {
+        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += 0.1) {
+            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += 0.1) {
+                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += 0.1) {
                     if (RandomUtil.getDouble(0, 100) <= chance) {
                         Location particleLocation = new Location(center.getWorld(), x, y, z);
                         if (particleLocation.distance(center) <= range) {
@@ -20,9 +20,9 @@ public class ParticleUtil {
     }
 
     public static void createColoredParticleArea(Location center, double range, Particle particle, double chance, Particle.DustOptions dustOptions, Color color) {
-        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += 0.5) {
-            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += 0.5) {
-                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += 0.5) {
+        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += 0.1) {
+            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += 0.1) {
+                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += 0.1) {
                     if (RandomUtil.getDouble(0, 100) <= chance) {
                         Location particleLocation = new Location(center.getWorld(), x, y, z);
                         if (particleLocation.distance(center) <= range) {
