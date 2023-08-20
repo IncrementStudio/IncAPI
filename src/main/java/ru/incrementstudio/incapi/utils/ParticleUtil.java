d35fad219a9d29f0698c9b41146bbaa9000d1e77
SUHARIK <sukhoveev.roma@gmail.com>
2023-08-21 05:46:37 +1000

    [!] Upgrade ParticleUtil

--- src/main/java/ru/incrementstudio/incapi/utils/ParticleUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/ParticleUtil.java
@@ -1,16 +1,33 @@
 package ru.incrementstudio.incapi.utils;
 
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 public class ParticleUtil {
-    public static void createParticleArea(Location center, double range, Particle particle) {
+    public static void createParticleArea(Location center, double range, Particle particle, double chance) {
         for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += 0.5) {
             for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += 0.5) {
                 for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += 0.5) {
-                    if (RandomUtil.getInt(0, 100) < 10) {
+                    if (RandomUtil.getDouble(0, 100) <= chance) {
                         Location particleLocation = new Location(center.getWorld(), x, y, z);
                         if (particleLocation.distance(center) <= range) {
-                            center.getWorld().spawnParticle(particle, particleLocation, 5);
+                            center.getWorld().spawnParticle(particle, particleLocation, 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void createColoredParticleArea(Location center, double range, Particle particle, double chance, Particle.DustOptions dustOptions, Color color) {
+        for (double x = center.getBlockX() - range; x <= center.getBlockX() + range; x += 0.5) {
+            for (double y = center.getBlockY() - range; y <= center.getBlockY() + range; y += 0.5) {
+                for (double z = center.getBlockZ() - range; z <= center.getBlockZ() + range; z += 0.5) {
+                    if (RandomUtil.getDouble(0, 100) <= chance) {
+                        Location particleLocation = new Location(center.getWorld(), x, y, z);
+                        if (particleLocation.distance(center) <= range) {
+                            center.getWorld().spawnParticle(particle, particleLocation, 0,
+                                    color.getRed() / 255.0, color.getGreen() / 255.0, color.getBlue() / 255.0, dustOptions);
                         }
                     }
                 }
