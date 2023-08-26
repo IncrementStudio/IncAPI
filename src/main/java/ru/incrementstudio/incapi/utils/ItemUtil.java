2e7df6ced1c2c1e2d314fd8d81ae9736a618cec3
ItzElite <semen.glushkov.08@mail.ru>
2023-08-26 16:24:57 +1000

    calcItemStackInPlayerInventory() in ItemUtil.java

--- src/main/java/ru/incrementstudio/incapi/utils/ItemUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/ItemUtil.java
@@ -134,4 +134,16 @@ public class ItemUtil {
         item.setItemMeta(skull);
         return item;
     }
+
+    public static int calcItemStackInPlayerInventory(Player player, ItemStack itemStack) {
+        int count = 0;
+        for (int i = 0; i < player.getInventory().getSize(); ++i) {
+            ItemStack stack;
+            if (i == 40 || i == 38 || i == 37 || i == 36 || i == 39 || (stack = player.getInventory().getItem(i)) == null || !stack.isSimilar(itemStack)) continue;
+            count += stack.getAmount();
+        }
+        return count;
+    }
+
+
 }
