8fbf89cd2f6656e91385108a809e41b91e95e7b2
ItzElite <semen.glushkov.08@mail.ru>
2023-10-26 18:40:39 +1000

    [/] RegionUtil new methods

--- src/main/java/ru/incrementstudio/incapi/utils/RegionUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/RegionUtil.java
@@ -8,18 +8,21 @@ import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class RegionUtil {
     public static RegionManager getRegionManager(World world) {
         return WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world));
     }
     public static boolean isRegionsExistsAt(World world, int x, int y, int z) {
         try {
-            return Objects.requireNonNull(WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(world))).getApplicableRegions(BlockVector3.at(x, y, z)).size() > 0;
+            return getRegionOnLocation(new Location(world, x, y, z)) != null;
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -39,11 +42,23 @@ public class RegionUtil {
     }
     public static List<ProtectedRegion> getRegionsInWorld(World world) {
         List<ProtectedRegion> readyList = new ArrayList<>();
-        if (WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(world)).getRegions().size() == 0) return readyList;
+        if (WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(world)).getRegions().isEmpty()) return readyList;
         for (Map.Entry<String, ProtectedRegion> map: WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(world)).getRegions().entrySet()) {
             if (!map.getValue().isPhysicalArea()) continue;
             readyList.add(map.getValue());
         }
         return readyList;
+    }
+
+    public static ProtectedRegion getRegionOnPlayerLocation(Player player) {
+        return getRegionOnLocation(player.getLocation());
+    }
+
+    public static ProtectedRegion getRegionOnLocation(Location location) {
+        List<ProtectedRegion> regions = new ArrayList<>(getRegionsInWorld(location.getWorld()));
+        for (ProtectedRegion protectedRegion: regions) {
+            if (protectedRegion.isPhysicalArea()) return protectedRegion;
+        }
+        return null;
     }
 }
