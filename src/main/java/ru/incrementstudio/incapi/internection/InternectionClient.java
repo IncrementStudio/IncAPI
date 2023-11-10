3bcdfbc03adbc295d0800cba0c6f10247376724d
SUHARIK <sukhoveev.roma@gmail.com>
2023-11-10 21:05:15 +1000

    [!] Threads logic

--- src/main/java/ru/incrementstudio/incapi/internection/InternectionClient.java
+++ src/main/java/ru/incrementstudio/incapi/internection/InternectionClient.java
@@ -1,16 +1,12 @@
 package ru.incrementstudio.incapi.internection;
 
-import ru.incrementstudio.incapi.Main;
-
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.*;
 
-public class InternectionClient {
+public class InternectionClient extends Thread {
     private final int port;
-    private final Thread receiveThread;
-    private final Thread sendThread;
     private final Deque<byte[]> sendQueue = new ArrayDeque<>();
     private final Deque<OneTimeHandler> handlers = new ArrayDeque<>();
     private static final byte[] closeCode = new byte[] { -128, -128, -128, -128 };
@@ -18,86 +14,63 @@ public class InternectionClient {
     private boolean closed = false;
     public InternectionClient(int port) {
         this.port = port;
-        sendThread = new Thread(this::send, "Inc_ClientSend_" + port);
-        receiveThread = new Thread(this::receive, "Inc_ClientRecv_" + port);
     }
     public void addOneTimeHandler(OneTimeHandler handler) {
         handlers.add(handler);
     }
-    private void receive() {
+
+    @Override
+    public void run() {
         DataInputStream in;
-        try {
-            in = new DataInputStream(socket.getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (socket.isConnected()) {
-                    try {
-                        int length = in.readInt();
-                        if (length > 0) {
-                            byte[] data = new byte[length];
-                            in.read(data, 0, length);
-                            if (handlers.isEmpty())
-                                dataHandler(data);
-                            else {
-                                OneTimeHandler handler = handlers.remove();
-                                handler.dataHandler(data);
-                            }
-                        }
-                    } catch (SocketException | EOFException e) {
-                        send(closeCode);
-                        cancel();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, 0L, 10L);
-    }
-    private void send() {
         DataOutputStream out;
         try {
+            in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (!socket.isClosed()) {
-                    if (!sendQueue.isEmpty()) {
-                        byte[] data = sendQueue.remove();
-                        try {
-                            if (!Arrays.equals(data, closeCode)) {
-                                out.writeInt(data.length);
-                                out.write(data);
-                                out.flush();
-                            } else {
-                                cancel();
+        while (!socket.isClosed()) {
+            if (!sendQueue.isEmpty()) {
+                byte[] data = sendQueue.remove();
+                try {
+                    if (!Arrays.equals(data, closeCode)) {
+                        out.writeInt(data.length);
+                        out.write(data);
+                        out.flush();
+
+                        int length = in.readInt();
+                        if (length > 0) {
+                            byte[] readData = new byte[length];
+                            in.read(readData, 0, length);
+                            if (handlers.isEmpty())
+                                dataHandler(readData);
+                            else {
+                                OneTimeHandler handler = handlers.remove();
+                                handler.dataHandler(readData);
                             }
-                        } catch (SocketException ignored) {
-                        } catch (IOException e) {
-                            e.printStackTrace();
                         }
+                    } else {
+                        break;
                     }
-                } else {
-                    cancel();
+                } catch (SocketException ignored) {
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-        }, 0L, 10L);
+        }
+        try {
+            close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
     public void send(byte[] data) {
         sendQueue.add(data);
     }
     public void bind() throws IOException {
         socket = new Socket("localhost", port);
-        receiveThread.start();
-        sendThread.start();
+        start();
     }
     public void dataHandler(byte[] data) { }
     public void close() throws IOException {
--- src/main/java/ru/incrementstudio/incapi/internection/InternectionServer.java
+++ src/main/java/ru/incrementstudio/incapi/internection/InternectionServer.java
@@ -1,7 +1,5 @@
 package ru.incrementstudio.incapi.internection;
 
-import ru.incrementstudio.incapi.Main;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,48 +8,42 @@ import java.util.*;
 
 public abstract class InternectionServer extends Thread {
     private final ServerSocket server;
-    private final List<ConnectionHandler> handlers = new ArrayList<>();
     public InternectionServer(int port) throws IOException {
-        super("Inc_Server_" + port);
         server = new ServerSocket(port);
         start();
     }
 
     @Override
     public void run() {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (!server.isClosed()) {
-                    try {
-                        Socket connection = server.accept();
-                        if (connection.isConnected()) {
-                            ConnectionHandler clientHandler = new ConnectionHandler(connection) {
-                                @Override
-                                public void dataHandler(byte[] data) {
-                                    InternectionServer.this.dataHandler(this, data);
-                                }
-                            };
-                            handlers.add(clientHandler);
+        while (!server.isClosed()) {
+            try {
+                Socket connection = server.accept();
+                if (connection.isConnected()) {
+                    new ConnectionHandler(connection) {
+                        @Override
+                        public void dataHandler(byte[] data) {
+                            InternectionServer.this.dataHandler(this, data);
                         }
-                    } catch (SocketException ignore) {
-                        cancel();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+                    };
                 }
+            } catch (SocketException ignore) {
+                break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        }, 0L, 10L);
+        }
+        try {
+            close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void dataHandler(ConnectionHandler handler, byte[] data) { }
     public void close() throws IOException {
         server.close();
     }
 
-    public static class ConnectionHandler {
-        private final Thread receiveThread;
-        private final Thread sendThread;
+    public static class ConnectionHandler extends Thread {
         private final Socket clientSocket;
         private boolean closed = false;
         private final Deque<byte[]> sendQueue = new ArrayDeque<>();
@@ -59,80 +51,61 @@ public abstract class InternectionServer extends Thread {
         private static final byte[] closeCode = new byte[] { -128, -128, -128, -128 };
         public ConnectionHandler(Socket connection) {
             clientSocket = connection;
-            receiveThread = new Thread(this::receive, "Inc_HandlerRecv_" + connection.getPort());
-            sendThread = new Thread(this::send, "Inc_HandlerSend_" + connection.getPort());
-            receiveThread.start();
-            sendThread.start();
+            start();
         }
         public void addOneTimeHandler(OneTimeHandler handler) {
             handlers.add(handler);
         }
-        private void receive() {
+        @Override
+        public void run() {
             DataInputStream in;
-            try {
-                in = new DataInputStream(clientSocket.getInputStream());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    if (clientSocket.isConnected() && !closed) {
-                        try {
-                            int length = in.readInt();
-                            if (length > 0) {
-                                byte[] data = new byte[length];
-                                in.read(data, 0, length);
-                                if (handlers.isEmpty())
-                                    dataHandler(data);
-                                else {
-                                    OneTimeHandler handler = handlers.remove();
-                                    handler.dataHandler(data);
-                                }
-                            }
-                        } catch (EOFException e) {
-                            send(closeCode);
-                            cancel();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }, 0L, 10L);
-        }
-        public void dataHandler(byte[] data) { }
-        private void send() {
             DataOutputStream out;
             try {
+                in = new DataInputStream(clientSocket.getInputStream());
                 out = new DataOutputStream(clientSocket.getOutputStream());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    if (!clientSocket.isClosed() && !closed) {
-                        if (!sendQueue.isEmpty()) {
-                            byte[] data = sendQueue.remove();
-                            try {
-                                if (!Arrays.equals(data, closeCode)) {
-
-                                    out.writeInt(data.length);
-                                    out.write(data);
-                                    out.flush();
-                                } else {
-                                    cancel();
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+            while (clientSocket.isConnected() && !closed) {
+                try {
+                    int length = in.readInt();
+                    if (length > 0) {
+                        byte[] data = new byte[length];
+                        in.read(data, 0, length);
+                        if (handlers.isEmpty())
+                            dataHandler(data);
+                        else {
+                            OneTimeHandler handler = handlers.remove();
+                            handler.dataHandler(data);
                         }
                     }
+
+                    if (!sendQueue.isEmpty()) {
+                        byte[] data = sendQueue.remove();
+                        try {
+                            if (!Arrays.equals(data, closeCode)) {
+                                out.writeInt(data.length);
+                                out.write(data);
+                                out.flush();
+                            } else {
+                                close();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        out.writeInt(0);
+                        out.flush();
+                    }
+                } catch (EOFException e) {
+                    send(closeCode);
+                    close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }, 0L, 10L);
+            }
         }
+        public void dataHandler(byte[] data) { }
         public void send(byte[] data) {
             sendQueue.add(data);
         }
