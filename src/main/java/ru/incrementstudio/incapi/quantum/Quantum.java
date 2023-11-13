e7c4de27c1639101a377d7f5d2b1a0e245e3dbe4
SUHARIK <sukhoveev.roma@gmail.com>
2023-11-13 18:54:48 +1000

    [!] Quantum

--- src/main/java/ru/incrementstudio/incapi/quantum/Quantum.java
+++ src/main/java/ru/incrementstudio/incapi/quantum/Quantum.java
@@ -3,7 +3,7 @@ package ru.incrementstudio.incapi.quantum;
 import java.util.*;
 import java.util.function.Predicate;
 
-public abstract class Quantum {
+public class Quantum {
     private final Map<Integer, Predicate<byte[]>> listeners = new HashMap<>();
     public Map<Integer, Predicate<byte[]>> getListeners() { return listeners; }
 
