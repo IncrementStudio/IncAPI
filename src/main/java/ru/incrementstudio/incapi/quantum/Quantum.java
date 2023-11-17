6901e5c64d47128de1a799099eefd8d9e8e17eb8
SUHARIK <sukhoveev.roma@gmail.com>
2023-11-17 18:16:19 +1000

    [!] Quantum data type #2

--- src/main/java/ru/incrementstudio/incapi/quantum/Quantum.java
+++ src/main/java/ru/incrementstudio/incapi/quantum/Quantum.java
@@ -4,15 +4,15 @@ import java.util.*;
 import java.util.function.Consumer;
 
 public class Quantum {
-    private final Map<Integer, Consumer<Object>> listeners = new HashMap<>();
-    public Map<Integer, Consumer<Object>> getListeners() { return listeners; }
+    private final Map<Integer, Consumer<Object[]>> listeners = new HashMap<>();
+    public Map<Integer, Consumer<Object[]>> getListeners() { return listeners; }
 
-    public void setListener(int id, Consumer<Object> listener) {
+    public void setListener(int id, Consumer<Object[]> listener) {
         listeners.put(id, listener);
     }
 
-    public void send(int id, Object data) {
-        Consumer<Object> listener = listeners.get(id);
+    public void send(int id, Object... data) {
+        Consumer<Object[]> listener = listeners.get(id);
         listener.accept(data);
     }
 }
