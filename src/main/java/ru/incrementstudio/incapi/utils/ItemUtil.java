30726cd64ec49c3acf198e09bf5ac7d0f430c1b9
ItzElite <semen.glushkov.08@mail.ru>
2023-11-25 21:43:12 +1000

    [/] ItemUtil fix

--- src/main/java/ru/incrementstudio/incapi/utils/ItemUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/ItemUtil.java
@@ -21,7 +21,7 @@ public class ItemUtil {
     public static ItemStack getHead(OfflinePlayer player) {
         ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
         SkullMeta skull = (SkullMeta) item.getItemMeta();
-        if (player.getPlayer() != null) {
+        if (player != null && player.getPlayer() != null) {
             skull.setPlayerProfile(player.getPlayer().getPlayerProfile());
         }
         item.setItemMeta(skull);
