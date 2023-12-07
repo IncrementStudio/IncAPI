8fa2bd61027e5de0ccb20bdfb7de92f27b5c762f
Jone501 <jone501@mail.ru>
2023-12-07 21:20:23 +1000

    [+] Menu viewers

--- src/main/java/ru/incrementstudio/incapi/menu/Menu.java
+++ src/main/java/ru/incrementstudio/incapi/menu/Menu.java
@@ -3,7 +3,9 @@ package ru.incrementstudio.incapi.menu;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Menu {
     private final List<Page> pages = new ArrayList<>();
@@ -13,20 +15,24 @@ public class Menu {
     public Page getPage(int index) {
         return pages.get(index);
     }
+    private final Map<Player, Data> viewers = new HashMap<>();
+    public Map<Player, Data> getViewers() {
+        return viewers;
+    }
 
     public void show(Player player) {
-        getPage(0).show(player);
+        show(player, 0);
+    }
+    public void show(Player player, Data data) {
+        show(player, 0, data);
     }
     public void show(Player player, int page) {
+        show(player, page, new Data());
+    }
+    public void show(Player player, int page, Data data) {
+        viewers.put(player, data);
         getPage(page).show(player);
     }
-//    public void show(Player player, int page, Data data) {
-//        pages.get(page).show(player, data);
-//    }
-//
-//    public void show(Player player, int page, Data data, Consumer<Data> start, Consumer<Data> end) {
-//        pages.get(page).show(player, data, start, end);
-//    }
 
     public Menu addPage(Page page) {
         pages.add(page);
--- src/main/java/ru/incrementstudio/incapi/menu/MenuListener.java
+++ src/main/java/ru/incrementstudio/incapi/menu/MenuListener.java
@@ -1,13 +1,16 @@
 package ru.incrementstudio.incapi.menu;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+
+import java.util.Map;
 
 public class MenuListener implements Listener {
     @EventHandler
@@ -15,6 +18,7 @@ public class MenuListener implements Listener {
         Inventory inventory = event.getClickedInventory();
         if (inventory == null) return;
         InventoryHolder inventoryHolder = inventory.getHolder();
+        if (inventoryHolder == null) return;
         if (inventoryHolder instanceof PageInventoryHolder) {
             event.setCancelled(true);
             PageInventoryHolder pageInventoryHolder = (PageInventoryHolder) inventoryHolder;
@@ -33,6 +37,7 @@ public class MenuListener implements Listener {
     @EventHandler
     public void onDrag(InventoryDragEvent event) {
         InventoryHolder inventoryHolder = event.getInventory().getHolder();
+        if (inventoryHolder == null) return;
         if (inventoryHolder instanceof PageInventoryHolder)
             event.setCancelled(true);
     }
@@ -40,45 +45,27 @@ public class MenuListener implements Listener {
     @EventHandler
     public void onDrop(PlayerDropItemEvent event) {
         InventoryHolder inventoryHolder = event.getPlayer().getOpenInventory().getTopInventory().getHolder();
+        if (inventoryHolder == null) return;
         if (inventoryHolder instanceof PageInventoryHolder)
             event.setCancelled(true);
     }
 
+//    @EventHandler
+//    public void onClose(InventoryCloseEvent event) {
+//        InventoryHolder inventoryHolder = event.getInventory().getHolder();
+//        if (inventoryHolder instanceof PageInventoryHolder) {
+//            PageInventoryHolder pageInventoryHolder = (PageInventoryHolder) inventoryHolder;
+//        }
+//    }
+
     @EventHandler
-    public void onClose(InventoryCloseEvent event) {
-        InventoryHolder inventoryHolder = event.getInventory().getHolder();
+    public void onQuit(PlayerQuitEvent event) {
+        InventoryHolder inventoryHolder = event.getPlayer().getOpenInventory().getTopInventory().getHolder();
+        if (inventoryHolder == null) return;
         if (inventoryHolder instanceof PageInventoryHolder) {
             PageInventoryHolder pageInventoryHolder = (PageInventoryHolder) inventoryHolder;
-//            Player player = (Player) event.getPlayer();
-//            boolean isReopen = false;
-//            if (page.getViewers().get(player) != null && page.getViewers().get(player).getData() != null) {
-//                if (page.getViewers().get(player).getData().containsKey("reopen")) {
-//                    isReopen = (boolean) page.getViewers().get(player).getData().get("reopen");
-//                }
-//                if (!isReopen) {
-//                    if (page.getViewers().get(player).getData().containsKey("task")) {
-//                        if (page.getViewers().get(player).getData().get("task") instanceof BukkitTask) {
-//                            BukkitTask task = (BukkitTask) page.getViewers().get(player).getData().get("task");
-//                            if (task != null)
-//                                task.cancel();
-//                        }
-//                    }
-//                    if (page.getEndFunctions().get(player) != null) {
-//                        page.getEndFunctions().get(player).accept(page.getViewers().get(player));
-//                        page.getEndFunctions().remove(player);
-//                    }
-//                }
-//            }
-//            if (!isReopen) {
-//                page.getViewers().remove(player);
-//                if (page.getViewers().isEmpty()) {
-//                    MenuListener.pages.remove(page);
-//                }
-//            } else {
-//                if (page.getViewers().get(player) != null && page.getViewers().get(player).getData() != null) {
-//                    page.getViewers().get(player).removeData("reopen");
-//                }
-//            }
+            Map<Player, Data> viewers = pageInventoryHolder.getPage().getMenu().getViewers();
+            viewers.remove(event.getPlayer());
         }
     }
 }
