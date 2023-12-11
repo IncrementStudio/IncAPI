00af021eebefc1ae70b2497f11a69e94c8785b75
Jone501 <jone501@mail.ru>
2023-12-11 18:46:24 +1000

    [!] Data logic

--- src/main/java/ru/incrementstudio/incapi/menu/Data.java
+++ src/main/java/ru/incrementstudio/incapi/menu/Data.java
@@ -1,19 +1,15 @@
 package ru.incrementstudio.incapi.menu;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Data {
     private final Map<String, List<Object>> data = new HashMap<>();
     public List<Object> getData(String key) {
+        if (!data.containsKey(key))
+            data.put(key, new ArrayList<>());
         return data.get(key);
     }
     public void removeData(String key) {
         data.remove(key);
-    }
-    public void setData(String key, Object... values) {
-        data.put(key, Arrays.asList(values));
     }
 }
