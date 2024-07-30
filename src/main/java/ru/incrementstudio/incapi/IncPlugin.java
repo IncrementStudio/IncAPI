720b292c281f1e161ae696771f32cf0ee1c5e3e1
Jone501 <sukhoveev.roma@gmail.com>
2024-07-30 12:23:31 +1000

    [+] Logger Messages

--- src/main/java/ru/incrementstudio/incapi/IncPlugin.java
+++ src/main/java/ru/incrementstudio/incapi/IncPlugin.java
@@ -1,26 +1,11 @@
 package ru.incrementstudio.incapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import ru.incrementstudio.incapi.configs.ConfigManager;
-
-import java.util.List;
 
 public abstract class IncPlugin extends JavaPlugin {
     protected final Logger logger = new Logger(this);
     @Override
-    public java.util.logging.Logger getLogger() {
+    public Logger getLogger() {
         return logger;
-    }
-    public Logger logger() {
-        return logger;
-    }
-
-    protected ConfigManager configManager;
-    public ConfigManager getConfigManager() {
-        return configManager;
-    }
-
-    public IncPlugin(List<String> configs) {
-        configManager = new ConfigManager(this, configs);
     }
 }
--- src/main/java/ru/incrementstudio/incapi/Logger.java
+++ src/main/java/ru/incrementstudio/incapi/Logger.java
@@ -30,4 +30,43 @@ public class Logger extends java.util.logging.Logger {
     public void simple(String msg) {
         System.out.println(ColorUtil.toColor(msg));
     }
+
+    public static class Messages {
+        public static class EN {
+            public static final String ENABLING = "&fPlugin enabling&7...";
+            public static final String ENABLE = "&aPlugin enabled!";
+            public static final String DISABLING = "&fPlugin disabling&7...";
+            public static final String DISABLE = "&cPlugin disabled!";
+            public static final String CONFIGS = "&fLoading configurations&7...";
+            public static final String DEPENDENCIES = "&fLoading dependencies&7...";
+            public static final String COMMANDS = "&fCommand registration&7...";
+            public static final String EVENTS = "&fEvent registration&7...";
+            public static final String THREADS = "&fStarting threads&7...";
+            public static final String MODULES = "&fLoading modules&7...";
+            public static final String SYSTEM = "&fLoading systems&7...";
+            public static final String SETTINGS = "&fLoading settings&7...";
+            public static final String FILES = "&fLoading files&7...";
+            public static final String MESSAGES = "&fLoading messages&7...";
+            public static final String LANGUAGES = "&fLoading languages&7...";
+            public static final String DATABASES = "&fLoading databases&7...";
+        }
+        public static class RU {
+            public static final String ENABLING = "&fВключение плагина...";
+            public static final String ENABLE = "&aПлагин включен!";
+            public static final String DISABLING = "&fВыключение плагина...";
+            public static final String DISABLE = "&cПлагин выключен!";
+            public static final String CONFIGS = "&fЗагрузка конфигурации&7...";
+            public static final String DEPENDENCIES = "&fЗагрузка зависимостей&7...";
+            public static final String COMMANDS = "&fРегистрация команд&7...";
+            public static final String EVENTS = "&fРегистрация ивентов&7...";
+            public static final String THREADS = "&fЗапуск потоков&7...";
+            public static final String MODULES = "&fЗагрузка модулей&7...";
+            public static final String SYSTEM = "&fЗагрузка систем&7...";
+            public static final String SETTINGS = "&fЗагрузка настроек&7...";
+            public static final String FILES = "&fЗагрузка файлов&7...";
+            public static final String MESSAGES = "&fЗагрузка сообщений&7...";
+            public static final String LANGUAGES = "&fЗагрузка языков&7...";
+            public static final String DATABASES = "&fЗагрузка баз данных&7...";
+        }
+    }
 }
