bf5b949dbd6f812f0f7830626a7814bb2c51a698
Jone501 <sukhoveev.roma@gmail.com>
2024-08-03 17:47:42 +1000

    [+] Added clamp methods to MathUtil

--- src/main/java/ru/incrementstudio/incapi/utils/MathUtil.java
+++ src/main/java/ru/incrementstudio/incapi/utils/MathUtil.java
@@ -4,7 +4,24 @@ public class MathUtil {
     public static double lerp(double a, double b, double t) {
         return a * (1 - t) + b * t;
     }
+
     public static double inverseLerp(double a, double b, double t) {
         return (t - a) / (b - a);
+    }
+
+    public static double clamp(double min, double max, double value) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static float clamp(float min, float max, float value) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static int clamp(int min, int max, int value) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static long clamp(long min, long max, long value) {
+        return Math.max(min, Math.min(max, value));
     }
 }
