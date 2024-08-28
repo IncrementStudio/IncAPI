21692c2b2562567e86a63c95460791278b5af618
ItzElite <71828335+ItzElite@users.noreply.github.com>
2024-08-28 23:44:48 +1000

    [/] Menu data edit

--- src/main/java/ru/incrementstudio/incapi/menu/Data.java
+++ src/main/java/ru/incrementstudio/incapi/menu/Data.java
@@ -4,12 +4,15 @@ import java.util.*;
 
 public class Data {
     private final Map<String, List<?>> data = new HashMap<>();
-    public List<?> getData(String key) {
+    public List<?> getValue(String key) {
         if (!data.containsKey(key))
             data.put(key, new ArrayList<>());
         return data.get(key);
     }
-    public void removeData(String key) {
+    public void addValue(String key, List<?> value) {
+        data.put(key, value);
+    }
+    public void removeValue(String key) {
         data.remove(key);
     }
 }
